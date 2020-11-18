3165566b6e54da967236461fb625ce7770ccfbd2
romitdas18 <romitdas18@gmail.com>
2020-11-18 16:26:23 +0530

    updated

--- UserService/src/main/java/com/stackroute/UserService/UserServiceApplication.java
+++ UserService/src/main/java/com/stackroute/UserService/UserServiceApplication.java
@@ -1,5 +1,8 @@
 package com.stackroute.UserService;
 
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -15,6 +18,12 @@ import javax.print.attribute.standard.MediaSize;
 @SpringBootApplication
 public class UserServiceApplication {
 
+	@Value("${spring.rabbitmq.queue}")
+	String queue;
+
+	@Value("${spring.rabbitmq.exchange}")
+	String exchange;
+
 	@Value("${spring.rabbitmq.host}")
 	String host;
 
@@ -23,6 +32,17 @@ public class UserServiceApplication {
 
 	@Value("${spring.rabbitmq.password}")
 	String password;
+
+	@Bean
+	Queue queue() {
+		return new Queue(queue, true);
+	}
+
+
+	@Bean
+	Exchange myExchange() {
+		return ExchangeBuilder.directExchange(exchange).durable(true).build();
+	}
 
 	@Bean
 	public CachingConnectionFactory factory(){
