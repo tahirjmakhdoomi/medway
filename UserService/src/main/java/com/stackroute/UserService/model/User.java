5c51e72cbb43c5d8277d839287ea36958442b48c
romitdas18 <romitdas18@gmail.com>
2020-11-23 11:09:09 +0530

    added user service

---
+++ UserService/src/main/java/com/stackroute/UserService/model/User.java
@@ -0,0 +1,142 @@
+package com.stackroute.UserService.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import java.io.Serializable;
+
+@Entity
+@Table(uniqueConstraints={@UniqueConstraint(columnNames={"user_name","user_email","user_phone"})})
+@Document("users")
+public class User implements Serializable {
+
+    private String name;
+    private String user_name;
+    private String user_email;
+    private String user_password;
+    private long user_phone;
+    private String user_storeName;
+    private String user_city;
+    private int user_pin;
+    private String user_state;
+    private String role;
+
+    public User(String name,String user_name, String user_email, String user_password, long user_phone, String user_storeName, String user_city, int user_pin, String user_state,String role) {
+        this.name = name;
+        this.user_name = user_name;
+        this.user_email = user_email;
+        this.user_password = user_password;
+        this.user_phone = user_phone;
+        this.user_storeName = user_storeName;
+        this.user_city = user_city;
+        this.user_pin = user_pin;
+        this.user_state = user_state;
+        this.role = role;
+    }
+
+    public User() {
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getUser_email() {
+        return user_email;
+    }
+
+    public void setUser_email(String user_email) {
+        this.user_email = user_email;
+    }
+
+    public String getUser_password() {
+        return user_password;
+    }
+
+    public void setUser_password(String user_password) {
+        this.user_password = user_password;
+    }
+
+    public long getUser_phone() {
+        return user_phone;
+    }
+
+    public void setUser_phone(long user_phone) {
+        this.user_phone = user_phone;
+    }
+
+    public String getUser_storeName() {
+        return user_storeName;
+    }
+
+    public void setUser_storeName(String user_storeName) {
+        this.user_storeName = user_storeName;
+    }
+
+    public String getUser_city() {
+        return user_city;
+    }
+
+    public void setUser_city(String user_city) {
+        this.user_city = user_city;
+    }
+
+    public int getUser_pin() {
+        return user_pin;
+    }
+
+    public void setUser_pin(int user_pin) {
+        this.user_pin = user_pin;
+    }
+
+    public String getUser_state() {
+        return user_state;
+    }
+
+    public void setUser_state(String user_state) {
+        this.user_state = user_state;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "user_name='" + user_name + '\'' +
+                ", user_email='" + user_email + '\'' +
+                ", user_password='" + user_password + '\'' +
+                ", user_phone=" + user_phone +
+                ", user_storeName='" + user_storeName + '\'' +
+                ", user_city='" + user_city + '\'' +
+                ", user_pin=" + user_pin +
+                ", user_state='" + user_state + '\'' +
+                '}';
+    }
+}
---
+++ UserService/src/main/java/com/stackroute/UserService/service/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package com.stackroute.UserService.service;
+
+import com.stackroute.UserService.model.User;
+import com.stackroute.UserService.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    UserRepository repository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public User saveUser(User user) {
+        if(user.getUser_storeName()==null|| user.getUser_storeName().equals("")){
+            user.setRole("Customer");
+        }
+        else {
+            user.setRole("Supplier");
+        }
+        return repository.save(user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return repository.findAll();
+    }
+}
