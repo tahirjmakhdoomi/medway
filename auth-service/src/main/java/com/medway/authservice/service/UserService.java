fef19e42c87956d8bdb916ac863f8b6bceb65be5
Get Authorized From Dev <Prashanth.P@stackroute.com>
2020-11-23 08:38:27 +0530

    merged auth-service codes

---
+++ auth-service/src/main/java/com/medway/authservice/service/UserService.java
@@ -0,0 +1,8 @@
+package com.medway.authservice.service;
+
+import com.medway.authservice.model.User;
+
+public interface UserService {
+    public User add(User user);
+    public User find(String username, String password);
+}
