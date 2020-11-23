bf7f61a7c0f9df2cf8cef0ef391388112bf9be2f
Get Authorized From Dev <Prashanth.P@stackroute.com>
2020-11-23 12:38:48 +0530

    added web app app component

---
+++ medway-webapp/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LandingViewComponent } from './landing-view/landing-view.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { UserTableComponent } from './user-table/user-table.component';
+
+const routes: Routes = [
+    {path : '',redirectTo : 'home',pathMatch: 'full'},
+    {path : 'home' , component : LandingViewComponent},
+    {path : 'signup' ,component : SignupComponent},
+    {path : 'login' , component: LoginComponent},
+    {path : 'addmedicine' , component : UserTableComponent},
+    {path : '**' , redirectTo : 'home'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
