7025d8cb7e10fa2f080f981af0a39530fc4a760f
Get Authorized From Dev <Prashanth.P@stackroute.com>
2020-11-25 08:55:59 +0530

    dockerized services

--- Medicine-Service/src/main/java/com/stackroute/Medicine/Service/MedicineServiceApplication.java
+++ Medicine-Service/src/main/java/com/stackroute/Medicine/Service/MedicineServiceApplication.java
@@ -2,10 +2,12 @@ package com.stackroute.Medicine.Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MedicineServiceApplication {
 
 	public static void main(String[] args) {
